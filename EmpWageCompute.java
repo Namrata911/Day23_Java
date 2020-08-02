ba47127f073561cfc584b21cb2197791c3c20d13
Namrata <911npm@gmail.com>
2020-08-02 12:40:14 +0530

    Added code for UC1 to check if employee is present

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -2,6 +2,17 @@ public class EmpWageCompute{
 
 public static void main(String[] args){
 	System.out.println("Welcome");
+	int isFullTime = 1;
+
+		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		System.out.println(empCheck);
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 
 }
 
